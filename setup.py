08c5ee2b63444229da0e380fa5d7e353d368392d
sunwei <me@sunwei.xyz>
2019-07-25 23:14:39 +0800

    Compare other; Wayde

--- setup.py
+++ setup.py
@@ -7,10 +7,6 @@ try:
 except ImportError:
     from distutils.core import setup
 
-HERE = os.path.abspath(os.path.dirname(__file__))
-ABOUT = {}
-with open(os.path.join(HERE, 'ddd_base', '__version__.py')) as f:
-    exec(f.read(), ABOUT)
 
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
@@ -19,10 +15,6 @@ CLASSIFIERS = [
     'Natural Language :: English',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
     'Topic :: Software Development :: Libraries :: Python Modules'
 ]
 
